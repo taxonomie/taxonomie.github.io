e26b4e7933b9abd83361728a57c6e6af40862fde
taxonomie <66442387+taxonomie@users.noreply.github.com>
2020-06-23 11:57:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 // calcul de la position des éléments 
-var largeurMenu = document.getElementById("menuPanel").offsetWidth + 'px';
-console.log(largeurMenu);
-document.getElementById("page").style.setProperty("left", largeurMenu);
+//var largeurMenu = document.getElementById("menuPanel").offsetWidth + 'px';
+//console.log(largeurMenu);
+//document.getElementById("page").style.setProperty("left", largeurMenu);
 
 
 menu = document.getElementById("menu").getElementsByTagName('div');
@@ -27,7 +27,6 @@ for (var i = categorie.length - 1; i >= 0; i--) {
 });
 }
 
-
 function hasClassName(elmt, className)
 {
    if(typeof elmt == "string")
@@ -35,5 +34,21 @@ function hasClassName(elmt, className)
    var regex = new RegExp("\\b" + className + "\\b");
    return regex.test(elmt.className);
 }
+
+$("#boutonMenu").click( function() {
+
+	var classe = $(this)[0].getElementsByTagName('i')[0].classList;
+	if( classe == "fas fa-bars" ){
+		classe.remove("fa-bars");
+		classe.add("fa-times");
+		document.getElementById('menuPanel').style.setProperty("display",'flex');
+	}
+	else {
+		classe.remove("fa-times");
+		classe.add("fa-bars");
+		document.getElementById('menuPanel').style.setProperty("display",'none');
+	}
+	
+});  
 
 });
