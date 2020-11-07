22f9953d460d4b5c9972d31b34ba4cbbcc91c340
taxonomie <66442387+taxonomie@users.noreply.github.com>
2020-11-07 17:10:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ menu = document.getElementById("menu").getElementsByTagName('div');
 categorie = []
 for (var i = menu.length - 1; i >= 0; i--) {
 	categorie.push(menu[i].id);
-	nbre = document.getElementsByClassName(menu[i].id).length;
-	console.log(document.getElementById(menu[i].id).getElementsByTagName('span')[0]);
+	if (menu[i].id == 'ALL') {
+		nbre = document.getElementsByClassName('source').length;
+	} else {
+		nbre = document.getElementsByClassName(menu[i].id).length;
+		console.log(document.getElementById(menu[i].id).getElementsByTagName('span')[0]);
+	};
 	document.getElementById(menu[i].id).getElementsByTagName('span')[0].innerHTML = nbre;
 }
 
-
-
-for (var i = categorie.length - 1; i >= 0; i--) {
-	identifiant = '#' + categorie[i]
+for (var i = categorie.length-1; i >=0; i--) {
+	identifiant = '#' + categorie[i];
 	$(identifiant).click( function() {
 		classe = $(this).attr('id');
 		liste = document.getElementById("liste").getElementsByTagName('a');
@@ -22,7 +24,7 @@ for (var i = categorie.length - 1; i >= 0; i--) {
 	 	};
 	 	for (i = 0; i<articles.length; i++) {
 	 		articles[i].style.setProperty("display",'flex');
-	 	}; 
+	 	};
 	});
 }
 
@@ -33,5 +35,20 @@ function hasClassName(elmt, className)
    var regex = new RegExp("\\b" + className + "\\b");
    return regex.test(elmt.className);
 }
+
+$("#boutonMenu").click( function() {
+	var classe = $(this)[0].getElementsByTagName('i')[0].classList;
+	if( classe == "fas fa-bars" ){
+		classe.remove("fa-bars");
+		classe.add("fa-times");
+		document.getElementById('menuPanel').style.setProperty("display",'flex');
+	}
+	else {
+		classe.remove("fa-times");
+		classe.add("fa-bars");
+		document.getElementById('menuPanel').style.setProperty("display",'none');
+	}
+	
+});
 
 });
